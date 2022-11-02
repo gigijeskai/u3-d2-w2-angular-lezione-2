9b624d8df743d59d076a131354a3f9518c08ebd0
Luigi Indaco <luigindaco@gmail.com>
2022-11-02 16:42:35 +0100

    rimozione nota

---
+++ angular-d-2/src/app/interfaces/inotes.ts
@@ -0,0 +1,4 @@
+export interface INotes {
+  name: string;
+  id: number;
+}
--- angular-d-2/src/app/page3/page3.component.ts
+++ angular-d-2/src/app/page3/page3.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { INotes } from '../interfaces/inotes';
 
 @Component({
   selector: 'app-page3',
   templateUrl: './page3.component.html',
-  styleUrls: ['./page3.component.scss']
+  styleUrls: ['./page3.component.scss'],
 })
 export class Page3Component implements OnInit {
+  title: string = 'TO Do List';
 
-  constructor() { }
+  notes: INotes[] = [
+    { name: 'buttare il vetro', id: 1 },
+    { name: 'fare la spesa', id: 2 },
+    { name: 'fare la corte', id: 3 },
+    { name: 'leggere le email', id: 4 },
+    { name: 'raccogliere le olive', id: 5 },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+  removeNote(obj: INotes): void {
+    let i = this.notes.indexOf(obj);
+    this.notes.splice(i, 1);
+    alert('hai rimosso la nota: ' + obj.id);
   }
-
 }
